7b4bf106a26a264c73c88549bccab53911a7261d
bhuvan <bhuvanshettyy@gmail.com>
2025-08-22 11:03:04 +0530

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,9 +1,9 @@
 # Configuration and constants used in the script. Update values as needed.
 
-USERNAME = "your_naukri_username"
-PASSWORD = "your_naukri_password"
-MOBILE = "your_naukri_phone"
-ORIGINAL_RESUME_PATH="your-resume-file-path/Resume.pdf"
-MODIFIED_RESUME_PATH="modified-resume-file-path/Resume.pdf"
+USERNAME = "bhuvanshettyy@gmail.com"
+PASSWORD = "agastya1212"
+MOBILE = "8296037465"
+ORIGINAL_RESUME_PATH=r"C:\Users\bhuva\OneDrive\Documents\Career\IT\RESUMES\Bhuvan_Resume.pdf"
+MODIFIED_RESUME_PATH=r"C:\Users\bhuva\OneDrive\Documents\Career\IT\RESUMES\Bhuvan_Resume.pdf"
 NAUKRI_LOGIN_URL="https://www.naukri.com/nlogin/login"
 NAUKRI_PROFILE_URL="https://www.naukri.com/mnjuser/profile"
